0ba78b2d7708fdfa8eac37bae4a7cebb947f2e71
Moses Gitau <gitaumoses4@gmail.com>
2024-02-13 12:41:50 +0300

    Fix failing build

--- apps/docs/src/app/Testing.tsx
+++ apps/docs/src/app/Testing.tsx
@@ -1,28 +1,12 @@
-import { myra, useColorPalette } from '@myra-ui/react';
+import { myra } from '@myra-ui/react';
 import React from 'react';
 import { useTheme } from 'next-themes';
 
-const backgrounds = [
-  'bg-action-1',
-  'bg-action-2',
-  'bg-action-3',
-  'bg-action-4',
-  'bg-action-5',
-  'bg-action-6',
-  'bg-action-7',
-  'bg-action-8',
-  'bg-action-9',
-  'bg-action-10',
-  'bg-action-11',
-  'bg-action-12',
-];
-
 export const Testing: React.FC<TestingProps> = (props) => {
   const { theme, setTheme } = useTheme();
-  const [palette, elementRef] = useColorPalette(theme || 'light');
 
   return (
-    <myra.div className="h-screen flex flex-col" actionColor="pink" ref={elementRef}>
+    <myra.div className="h-screen flex flex-col" actionColor="pink">
       {theme && (
         <div className="h-12 text-white bg-black items-center justify-center flex">
           <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>{theme[0].toUpperCase() + theme.slice(1)}</button>
@@ -34,7 +18,7 @@ export const Testing: React.FC<TestingProps> = (props) => {
         <div className="items-center justify-center flex bg-foreground-8">Foreground</div>
       </myra.div>
       <p>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox</p>
-      <div className="h-24 w-24 m-4" style={{ background: palette.action['8'] }}>
+      <div className="h-24 w-24 m-4" style={{ background: 'red' }}>
         Some box
       </div>
     </myra.div>
--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -2,8 +2,6 @@ const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 const myraUIPlugin = require('@myra-ui/theme/plugin');
 
-console.log(myraUIPlugin);
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'), ...createGlobPatternsForDependencies(__dirname)],
