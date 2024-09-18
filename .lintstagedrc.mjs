73d3670fed928c2b16ea4abfa6fcbba114095deb
Moses Gitau <gitaumoses4@gmail.com>
2024-09-18 14:55:43 +0300

    chore(format): update commit hooks

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,25 @@
+import { relative } from 'path';
+import { ESLint } from 'eslint';
+
+const removeIgnoredFiles = async (files) => {
+  const cwd = process.cwd();
+  const eslint = new ESLint();
+  const relativePaths = files.map((file) => relative(cwd, file));
+  const isIgnored = await Promise.all(relativePaths.map((file) => eslint.isPathIgnored(file)));
+  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+
+  return filteredFiles.join(' ');
+};
+
+export default {
+  '**/*.{js,ts,jsx,tsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+
+    return [`eslint -c .eslintrc.json --max-warnings=0 --fix ${filesToLint}`];
+  },
+  '**/*.css': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+
+    return [`prettier --config .prettierrc --ignore-path --write ${filesToLint}`];
+  },
+};
