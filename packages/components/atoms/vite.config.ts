5b92acc3f3b9b89d223b8452ffa2f5756a50b266
Moses Gitau <gitaumoses4@gmail.com>
2024-09-18 13:54:04 +0300

    Configure release

--- packages/components/atoms/vite.config.ts
+++ packages/components/atoms/vite.config.ts
@@ -7,7 +7,6 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../../node_modules/.vite/packages/components/atoms',
-
   plugins: [
     react(),
     nxViteTsPaths(),
