0a400f9503e6db09f957745b6464e03abc53acfa
Moses Gitau <gitaumoses4@gmail.com>
2024-09-19 09:29:27 +0300

    chore(release): fix packages release (#37)

--- packages/components/templates/vite.config.ts
+++ packages/components/templates/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: './dist',
+    outDir: '../../../dist/packages/components/templates',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
