9690aae0adf289b6d0fc04f567c68e8fb7701f19
Moses Gitau <gitaumoses4@gmail.com>
2024-10-02 11:21:49 +0300

    fix(tests): fix failing tests for toast component

--- packages/components/__tests__/molecules/toast.spec.tsx
+++ packages/components/__tests__/molecules/toast.spec.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Toast } from '../../src/molecules';
+import { ToastProvider } from '@radix-ui/react-toast';
 
 describe('Toast', () => {
   it('should render correctly', () => {
-    const wrapper = render(<Toast />);
+    const wrapper = render(
+      <ToastProvider>
+        <Toast />
+      </ToastProvider>
+    );
 
     expect(() => wrapper.unmount()).not.toThrow();
   });
