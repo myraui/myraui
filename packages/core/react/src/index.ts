678d849abc911c076db869a6a678c525a472527b
Moses Gitau <gitaumoses4@gmail.com>
2024-10-12 08:14:44 +0300

    feat(react): update react package dependencies

--- packages/core/react/src/index.ts
+++ packages/core/react/src/index.ts
@@ -3,4 +3,5 @@ export * from '@myraui/components';
 // export * from '@myraui/hooks';
 export * from '@myraui/react-utils';
 export * from '@myraui/shared-utils';
-// export * from '@myraui/theme';
+export * from '@myraui/theme';
+export * from '@myraui/server';
